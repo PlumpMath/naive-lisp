9f285c3781799a3f009e225773cff42b3c35cdea
Erik <erik.svedang@gmail.com>
2014-01-13 00:21:26 +0100

    reader

---
+++ reader.h
@@ -0,0 +1,17 @@
+#ifndef READER
+#define READER
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <assert.h>
+#include <string.h>
+#include <ctype.h>
+
+#include "gc.h"
+
+
+
+
+
+#endif
